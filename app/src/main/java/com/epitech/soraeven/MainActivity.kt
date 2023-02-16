6236942ffa9a7dadb246046c37a01facf32ac124
Raeckelboom Pierre <pierre.raeckelboom@gmail.com>
2023-02-16 20:23:52 +0100

    FIX: Use the right Android version

--- app/src/main/java/com/epitech/soraeven/MainActivity.kt
+++ app/src/main/java/com/epitech/soraeven/MainActivity.kt
@@ -6,12 +6,16 @@ import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mLoginButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-    val loginButton = findViewById(R.id.loginButton) as Button
-    loginButton.setOnClickListener {
-        Toast.makeText(this.@MainActivity, "You clicked me", Toast.LENGTH_SHORT)
+
+        // val loginButton = findViewById(R.id.loginButton) as Button
+        mLoginButton = findViewById(R.id.loginButton) as Button
+        mLoginButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "You clicked me", Toast.LENGTH_SHORT).show()
+        }
     }
 }
