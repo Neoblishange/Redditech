a4045b3b11688e6793b249a17edbe3eb93df9bb3
Raeckelboom Pierre <pierre.raeckelboom@gmail.com>
2023-02-21 17:10:02 +0100

    FEAT: The user is brought to the Landing activity

--- app/src/main/java/com/epitech/soraeven/LandingActivity.kt
+++ app/src/main/java/com/epitech/soraeven/LandingActivity.kt
@@ -1,11 +1,23 @@
 package com.epitech.soraeven
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.epitech.soraeven.api.model.AccessToken
+import com.epitech.soraeven.api.service.RedditClient
+import okhttp3.Credentials
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import android.widget.Toast
+import retrofit2.Callback
+import retrofit2.Response
 
 class LandingActivity : AppCompatActivity() {
+    private val clientId = "DmJqy_hwmMfuC0b1YMn43g"
+    private val redirectUri = Uri.parse("soraeven://oauth2redirect")
     private lateinit var mNavigateHomeButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +28,46 @@ class LandingActivity : AppCompatActivity() {
         mNavigateHomeButton.setOnClickListener {
             val intent = Intent(this@LandingActivity, MainActivity::class.java)
             startActivity(intent)
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+
+        val uri: Uri? = intent.data
+        if (uri != null && uri.toString().startsWith(redirectUri.toString())) {
+            val code = uri.getQueryParameter("code")
+
+            val builder: Retrofit.Builder = Retrofit.Builder()
+                .baseUrl("https://www.reddit.com/api/v1/")
+                .addConverterFactory(GsonConverterFactory.create())
+
+            val retrofit: Retrofit = builder.build()
+
+            val client = retrofit.create(RedditClient::class.java)
+            val credentials: String = Credentials.basic(clientId, "")
+            val accessTokenCall: Call<AccessToken?>? =
+                code?.let {
+                    client.getAccessToken(credentials, "authorization_code", redirectUri.toString(),
+                        it
+                    )
+                }
+            if (accessTokenCall != null) {
+                accessTokenCall.enqueue(object: Callback<AccessToken?> {
+                    override fun onFailure(call: Call<AccessToken?>, t: Throwable) {
+                        Toast.makeText(this@LandingActivity,
+                            "No!", Toast.LENGTH_SHORT).show()
+                    }
+                    override fun onResponse(
+                        call: Call<AccessToken?>,
+                        response: Response<AccessToken?>
+                    ) {
+                        Toast.makeText(this@LandingActivity,
+                            "Yeah!", Toast.LENGTH_SHORT).show()
+                        // We can retrieve the access token by doing response.body()?.getAccessToken()
+                        println(response.body()?.getAccessToken())
+                    }
+                })
+            }
         }
     }
 }
--- app/src/main/java/com/epitech/soraeven/MainActivity.kt
+++ app/src/main/java/com/epitech/soraeven/MainActivity.kt
@@ -4,21 +4,13 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.epitech.soraeven.api.model.AccessToken
-import com.epitech.soraeven.api.service.RedditClient
-import okhttp3.Credentials
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mLoginButton: Button
     private lateinit var mRedirectButton: Button
     private val clientId = "DmJqy_hwmMfuC0b1YMn43g"
+    @Suppress("SpellCheckingInspection")
     private val redirectUri = Uri.parse("soraeven://oauth2redirect")
     private val scopes = Uri.parse("read save")
     private val authUrl = Uri.parse("https://www.reddit.com/api/v1/authorize.compact" +
@@ -33,61 +25,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // val loginButton = findViewById(R.id.loginButton) as Button
-        mLoginButton = findViewById<Button>(R.id.loginButton)
+        mLoginButton = findViewById(R.id.loginButton)
         mLoginButton.setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW, authUrl)
             startActivity(intent)
         }
 
-        // Navigate button
         mRedirectButton = findViewById(R.id.redirectButton)
         mRedirectButton.setOnClickListener {
             val i = Intent(this@MainActivity, LandingActivity::class.java)
             startActivity(i)
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        val uri: Uri? = intent.data
-        if (uri != null && uri.toString().startsWith(redirectUri.toString())) {
-            val code = uri.getQueryParameter("code")
-
-            val builder: Retrofit.Builder = Retrofit.Builder()
-                .baseUrl("https://www.reddit.com/api/v1/")
-                .addConverterFactory(GsonConverterFactory.create())
-
-            val retrofit: Retrofit = builder.build()
-
-            val client = retrofit.create(RedditClient::class.java)
-            val credentials: String = Credentials.basic(clientId, "")
-            val accessTokenCall: Call<AccessToken?>? =
-                code?.let {
-                    client.getAccessToken(credentials, "authorization_code", redirectUri.toString(),
-                        it
-                    )
-                }
-            if (accessTokenCall != null) {
-                accessTokenCall.enqueue(object: Callback<AccessToken?> {
-                    override fun onFailure(call: Call<AccessToken?>, t: Throwable) {
-                        Toast.makeText(this@MainActivity,
-                            "No!", Toast.LENGTH_SHORT).show()
-                    }
-                    override fun onResponse(
-                        call: Call<AccessToken?>,
-                        response: Response<AccessToken?>
-                    ) {
-                        Toast.makeText(this@MainActivity,
-                            "Yeah!", Toast.LENGTH_SHORT).show()
-                        // We can retrieve the access token by doing response.body()?.getAccessToken()
-                        println(response.body()?.getAccessToken())
-                    }
-                })
-            }
-
-
         }
     }
 }
