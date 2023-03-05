80e49927d656fdb4aa99579a18973050f3eda14d
Kévin Vendome <kevin.vendome@epitech.eu>
2023-03-05 23:11:42 +0100

    Comment Object

--- app/src/main/java/com/epitech/soraeven/object/comment/AfficheCommentPost.kt
+++ app/src/main/java/com/epitech/soraeven/object/comment/AfficheCommentPost.kt
@@ -1,3 +1,43 @@
 package com.epitech.soraeven.`object`.comment
 
-data class AfficheCommentPost()
+data class AfficheCommentPost(
+    var data: AfficheCommentPostData
+)
+
+data class AfficheCommentPostData(
+    var children: ChildrenAfficheCommentPost
+)
+
+data class ChildrenAfficheCommentPost(
+    var data: ChildrenAfficheCommentPostData
+)
+
+data class ChildrenAfficheCommentPostData(
+  var id: Int,
+  var author: String,
+  var created_utc: String,
+  var score: Int,
+  var body: ,
+  var replies: RepliesAfficheCommentPost
+)
+
+data class RepliesAfficheCommentPost(
+    var data: RepliesAfficheCommentPostData
+)
+
+data class RepliesAfficheCommentPostData(
+    var children: Array<ChildrenRepliesAfficheCommentPost>
+)
+
+data class ChildrenRepliesAfficheCommentPost(
+    var data: ChildrenRepliesAfficheCommentPostData
+)
+
+data class ChildrenRepliesAfficheCommentPostData(
+    var id: Int,
+    var author: String,
+    var created_utc: String,
+    var score: Int,
+    var body: ??,
+    var replies:{etc...}
+)
