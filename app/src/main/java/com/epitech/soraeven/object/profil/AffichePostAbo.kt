a9f89adbd5b779a7491733d9d98389f8e0e3f9be
Kévin Vendome <kevin.vendome@epitech.eu>
2023-03-05 23:00:43 +0100

    Profile Object

--- app/src/main/java/com/epitech/soraeven/object/profil/AffichePostAbo.kt
+++ app/src/main/java/com/epitech/soraeven/object/profil/AffichePostAbo.kt
@@ -1,3 +1,22 @@
 package com.epitech.soraeven.`object`.profil
 
-data class AffichePostAbo()
+data class AffichePostAbo(
+    var data: DataAffichePostAbo
+)
+
+data class DataAffichePostAbo(
+    var children: Array<ChildrenAffichePostAbo>
+)
+
+data class ChildrenAffichePostAbo(
+    var data: ChildrenAffichePostAboData
+)
+
+data class ChildrenAffichePostAboData(
+    var id: Int,
+    var subreddit_name_prefixed:String,
+    var title:String,
+    var author:String,
+    var thumbnail:String,
+    var num_comments:Int
+)
--- app/src/main/java/com/epitech/soraeven/object/profil/ProfilUser.kt
+++ app/src/main/java/com/epitech/soraeven/object/profil/ProfilUser.kt
@@ -1,3 +1,15 @@
 package com.epitech.soraeven.`object`.profil
 
-data class ProfilUser()
+data class ProfilUser(
+    var name: String,
+    var subreddit: SubredditProfilUser
+)
+
+data class SubredditProfilUser(
+    var subreddit_type: ??,
+    var title: String,
+    var public_description: String,
+    var banner_img: String,
+    var icon_img: String,
+    var created_utc: ??
+)
