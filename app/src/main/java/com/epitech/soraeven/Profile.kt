3455d4d9b0291d60b44e7af70c76ed8031f66514
Raeckelboom Pierre <pierre.raeckelboom@gmail.com>
2023-03-06 11:39:59 +0100

    WIP: Profile.kt

--- app/src/main/java/com/epitech/soraeven/Profile.kt
+++ app/src/main/java/com/epitech/soraeven/Profile.kt
@@ -1,11 +1,15 @@
 package com.epitech.soraeven
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class Profile : AppCompatActivity() {
+    private lateinit var mUserSettingsImage: ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.profile)
+        setContentView(R.layout.activity_profile)
+
+        //mUserSettingsImage = findViewById(settings_icon)
     }
 }
