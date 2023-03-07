ab97a96454edad2153e64fb63eb55440f5911bf6
Raeckelboom Pierre <pierre.raeckelboom@gmail.com>
2023-03-07 17:26:22 +0100

    WIP: Create Patch UserSettings

--- app/src/main/java/com/epitech/soraeven/controller/RedditInterface.kt
+++ app/src/main/java/com/epitech/soraeven/controller/RedditInterface.kt
@@ -24,4 +24,7 @@ interface RedditInterface {
     @Headers("Accept: application/json")
     @GET("/api/v1/me/prefs")
     fun getUserSettings(): Call<UserSettings>
+    @Headers("Accept: apllication/json")
+    @PATCH("/api/v1/me/prefs")
+    fun setUserSettings(@Body userSettings: UserSettings)
 }
